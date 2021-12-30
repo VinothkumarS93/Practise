5e6836f9295faef45022585c57e72878e2788a99
VinothkumarS93 <96652401+VinothkumarS93@users.noreply.github.com>
2021-12-30 09:39:50 +0530

    Add files via upload

---
+++ Output task2,3.py
@@ -0,0 +1,10 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/railu/Desktop/Task2,3-29.py ================
+25.12
+The perimeter of circle is 25.12
+the perimeter of circle for the radius 4 and pi 3.14 is 25.12
+the hieght of the cone is 301.44
+the hieght of the cone is 301
+the hieght of the cone for radius 4 and hieght 6 and pi 3.14 is
