872cb95209b697688ccaf45a88d8b963712cf2a9
VinothkumarS93 <96652401+VinothkumarS93@users.noreply.github.com>
2021-12-31 10:52:33 +0530

    Add files via upload

---
+++ Dec 30 Index Task 1.py
@@ -0,0 +1,23 @@
+#Information Technology
+a="Information Technology"
+print(a[10])
+print(a[-10])
+print(a[12])
+print(a[3])
+print(a[-5])
+print(a[5])
+print(a[0])
+print(a[-1])
+print(a[20])
+
+
+a=input("the value of input : ")
+print (int(len(a)/2))
+print(a[4])
+
+
+a=input("the value of input : ")
+print (int(len(a)//2))
+print(a[4])
+
+
