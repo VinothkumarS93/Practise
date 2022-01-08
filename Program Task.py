f61f781c639e630b71c1e8ad0743f326ec5cc63d
VinothkumarS93 <96652401+VinothkumarS93@users.noreply.github.com>
2022-01-08 09:13:52 +0530

    Add files via upload

---
+++ Program Task.py
@@ -0,0 +1,57 @@
+print ("Hello, World")
+
+'''a=int(3)
+b=int(5)
+c=(a+b)
+d=(a-b)
+e=(a*b)
+print(c)
+print(d)
+print(e)'''
+
+#Addition,Sub,Product
+a=int(3)
+b=int(2)
+c=(a+b)
+d=(a-b)
+e=(a*b)
+print(c)
+print(d)
+print(e)
+
+
+#Division,Floor Divivsion
+a=4
+b=3
+c=(4//3)
+d=(4/3)
+print(c)
+print(d)
+
+
+#print fn & ln in a line
+a=("Ross")
+b=("Taylor")
+print("Hello", a,end=" ")
+print(b,"!")
+
+
+#swapcase
+a=('HackerRank.com presents "Pythonist 2".')
+print(a.swapcase())
+
+#capitalize
+a=("chris alan")
+print(a.title())
+
+#replace
+a="abracadabra"
+print(int(a.find ('a')))
+print(a.replace('abraca','abrack'))
+
+print(a[:5]+"k"+a[6:])
+
+
+
+
+
