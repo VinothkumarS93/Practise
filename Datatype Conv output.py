0e3d994ae7e71269d8d58995cb0e6b88fe028bfe
VinothkumarS93 <96652401+VinothkumarS93@users.noreply.github.com>
2022-01-10 20:02:08 +0530

    Add files via upload

---
+++ Datatype Conv output.py
@@ -0,0 +1,13 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:/Users/railu/Desktop/Program conversion.py ============
+(1, 2, 'python', 4, 5, 6, 5)
+{1, 2, 4, 5, 6, 'python'}
+{1: 'python', 2: 10, 3: 100, 4: 200}
+[1, 2, 'python', 4, 5, 6, 5]
+{1, 2, 4, 5, 6, 'python'}
+{1: 'python', 2: 10, 'python': 100, 4: 200}
+[1, 2, 4, 5, 6, 'python']
+{1, 2, 4, 5, 6, 'python'}
+{1: 200, 2: 10, 4: 100, 5: 'python'}
---
+++ Nested DS output.py
@@ -0,0 +1,38 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/railu/Desktop/Python practise/Nested DS practise.py ====
+5
+56
+222
+5555
+50000
+7777
+666
+89
+on
+put
+333
+3333
+science
+Computer
+666
+201
+102
+999
+777
+ll
+thon
+11
+6666
+6666
+7777
+7777
+222
+7777
+[33, 44]
+py
+ello
+en
+zoo
+Bot
